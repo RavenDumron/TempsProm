8f6f497cc977133eb66646171d52586a8e69dd55
RavenDumron <miscellaneesalaclef@gmail.com>
2025-01-20 17:46:31 +0100

    Deletion of site_ref

--- site_ref/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start ();
-session_destroy ();
-require_once(__DIR__ . '/config/mysql.php');
-require_once(__DIR__ . '/databaseconnect.php');
-require_once(__DIR__ . '/variables.php');
-require_once(__DIR__ . '/functions.php');
-redirectToUrl('index.php');
